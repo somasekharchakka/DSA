db11fcc2ad314641659d8f658a1594c874deacfa
Somasekhar chakka <somasekharchakka9494@gmail.com>
2026-07-19 22:57:17 +0530

    Time: 0 ms (100%), Space: 12.3 MB (91.64%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+        """
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: None Do not return anything, modify nums1 in-place instead.
+        """
+
+        list1=nums1[:m]+nums2
+        list1.sort()
+        nums1[:]=list1
