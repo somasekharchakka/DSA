6ec0280f189afb0ffab92dc772e76200af87c226
Somasekhar chakka <somasekharchakka9494@gmail.com>
2026-07-25 16:19:22 +0530

    Add solution for Rotate Array

--- LeetCode/Rotate-Array/Solution.py
+++ LeetCode/Rotate-Array/Solution.py
@@ -1,5 +1,6 @@
 1class Solution(object):
 2    def rotate(self, nums, k):
-3        k=k%len(nums)
-4        nums[:]=nums[-k:]+nums[:-k]
-5      
+3        n=len(nums)
+4        k=k%n
+5        nums[:]=nums[-k:]+nums[:-k]
+6        
