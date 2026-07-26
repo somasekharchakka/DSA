f050ec7dc7aea658ea9832f9caf857a1a5eddc4b
Somasekhar chakka <somasekharchakka9494@gmail.com>
2026-07-26 21:34:42 +0530

    Add solution for Binary Search

---
+++ LeetCode/Binary-Search/Solution.py
@@ -0,0 +1,23 @@
+1class Solution(object):
+2    def search(self, nums, target):
+3        """
+4        :type nums: List[int]
+5        :type target: int
+6        :rtype: int
+7        """
+8        left=0
+9        right=len(nums)-1
+10        while left <= right:
+11            mid =(left+right)//2
+12
+13            if nums[mid] == target:
+14                return mid
+15
+16            elif nums[mid]<=target:
+17                left=mid+1
+18
+19            else:
+20                right=mid-1
+21
+22        return -1
+23
